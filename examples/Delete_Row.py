fa66c83eedf115b6b92c739d081318c472cf8e7a
HaoranSun1122 <ayshr@126.com>
2022-09-09 20:26:26 +0800

    0909

--- examples/Delete_Row.py
+++ examples/Delete_Row.py
@@ -1,6 +1,19 @@
 import pandas as pd
 
-df = pd.read_csv("Parsed_Parameter.csv")
-error_bad_lines=False #加入参数
-df.drop(df.index[11175630:23539049],inplace=True)
-df.to_csv("data_new.csv",index=False,encoding="utf-8")
+
+file1 = open('Parsed_Parameter.csv', 'r').readlines()
+fileout = open('Parsed_Parameter_New.csv', 'w')
+for line in file1:
+    # fileout.write(line.replace('"', ''))
+    print(fileout.write(line.replace('"', '')))
+fileout.close()
+
+
+
+
+
+# df = pd.read_csv("Parsed_Parameter.csv")
+#
+# error_bad_lines=False #加入参数
+# df.drop(df.index[11175630:14506674],inplace=True)
+# df.to_csv("data_new.csv",index=False,encoding="utf-8")
