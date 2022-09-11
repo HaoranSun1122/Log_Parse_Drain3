c4057a53934f070072264adcc4f6b6e8a93e5631
HaoranSun1122 <ayshr@126.com>
2022-09-11 23:41:33 +0800

    Update Delete_Row.py

--- examples/Delete_Row.py
+++ examples/Delete_Row.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import csv
-#
+
 # csvfile = open('Parsed_Parameter.csv',encoding='utf-8')
 # df = pd.read_csv(csvfile,engine='python')
 # print(range(len(df)))
@@ -9,6 +9,7 @@ import csv
 #     print(df[i])
 
 from csv import reader
+fileout = open('labels1.csv', 'w')
 
 # open file
 with open("Parsed_Parameter.csv", "r") as my_file:
@@ -17,12 +18,13 @@ with open("Parsed_Parameter.csv", "r") as my_file:
     # do this for all the rows
     for i in file_reader:
         # print the rows
-        print(type(i))
         print(i)
+        fileout.write(i.replace('"', ''))
+        print(i)
+fileout.close()
 
-
-df = pd.read_csv("Parsed_Parameter.csv")
-
-error_bad_lines=False #加入参数
-df.drop(df.index[200:300],inplace=True)
-df.to_csv("Parsed_Parameter.csv",index=False,encoding="utf-8")
+# df = pd.read_csv("Parsed_Parameter.csv")
+#
+# error_bad_lines=False #加入参数
+# df.drop(df.index[11175630:20743527],inplace=True)
+# df.to_csv("Parsed_Parameter.csv",index=False,encoding="utf-8")
